23d4d42c62055b92a1d92afa12f6f80fd29e9852
Richard Chukwuma <richardchukwuma99@gmail.com>
2021-10-01 18:35:23 +0100

    initial commit

---
+++ employee-management-api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace employee_management_api.Extensions
+{
+ public static class ServiceExtensions
+ {
+  public static void ConfigureCors(this IServiceCollection services) =>
+      services.AddCors(options =>
+      {
+       options.AddPolicy("CorsPolicy",
+             builder => builder
+             .AllowAnyOrigin()
+             .AllowAnyMethod()
+             .AllowAnyHeader());
+      });
+ }
+}
