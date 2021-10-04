3ff5508957c098a9d9f50403091f970679d55951
Richard Chukwuma <richardchukwuma99@gmail.com>
2021-10-04 03:24:22 +0100

    create repository user classes

---
+++ Contracts/Repositories/Implementations/CompanyRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Infrastructure.Data.DbContexts;
+using Infrastructure.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories.Implementations
+{
+    class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
+    {
+        public CompanyRepository(AppDbContext appDbContext)
+            : base(appDbContext)
+        {
+        }
+    }
+}
